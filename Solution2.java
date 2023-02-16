3698fa28b520d4cfcf7c885d4238a119f56fc489
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-02-16 22:06:18 +0530

    Addition ot two Number

---
+++ Solution2.java
@@ -0,0 +1,12 @@
+public class Solution2
+{
+	int sum(int a,int b){
+	     return a+b;	
+	}
+	public static void main(String[] args){
+		Solution2 solution=new Solution2();
+		int x=solution.sum(1,2);
+		System.out.println(x);
+		
+	}
+}
