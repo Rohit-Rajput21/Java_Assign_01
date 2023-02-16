4e32d7d51df2a89573ad9565c98a94f1273f514c
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-02-16 22:14:32 +0530

    Division of two Number

---
+++ Solution4.java
@@ -0,0 +1,12 @@
+public class Solution4{
+	
+	public double divide(double a,double b){
+		return a/b;
+	}
+	public static void main(String[] args)
+	{
+		Solution4 solution =new Solution4();
+		double x=solution.divide(5,2);
+		System.out.println(x);
+	}
+}
