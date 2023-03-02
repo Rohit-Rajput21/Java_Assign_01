f3d987852f3f81ef103fc68cbbc8e49849577104
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-03-02 22:00:48 +0530

    account class added

---
+++ inheritance_assign/LoanAccount.java
@@ -0,0 +1,11 @@
+public class LoanAccount extends Account{
+    public void payEMI(){
+		System.out.println("LoanAccount's payEMI method");
+	}
+    public void topUpLoan(){
+		System.out.println("LoanAccount's topUpLoan method");
+	}
+    public void repayment(){
+		System.out.println("LoanAccount's repayment method");
+	}
+}
---
+++ inheritance_assign/SavingAccount.java
@@ -0,0 +1,11 @@
+public class SavingAccount extends Account{
+    public void withdraw(){
+		System.out.println("SavingAccount's withdraw method");
+	}
+    public void deposit(){
+		System.out.println("SavingAccount's deposit method");
+	}
+    public void fixedDeposit(){
+		System.out.println("SavingAccount's fixedDeposit method");
+	}
+}
